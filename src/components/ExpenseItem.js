ea73544653125c8e2d5bb87fa02592a42977cbd1
elvis <cenelvis@gmail.com>
2022-05-19 18:38:56 -0400

    Splitting Components Into Multiple Components, Chaining (props) in multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,11 @@
-import "./ExpenseItem.css";
+import "./ExpenseItem.css"; // This is how CSS file is imported
+import ExpenseDate from "./ExpenseDate";
 
+// date={props.date} => was added so "App.js" can use its own data
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
