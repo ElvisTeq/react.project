92368f8fed74d71fb0ea31d72aefcea8708f2f7a
elvis <cenelvis@gmail.com>
2022-05-20 05:14:10 -0400

    How Component Function Are Executed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,8 +4,12 @@ import Card from "../UI/Card";
 
 // <ExpenseDate date={props.date} /> => Storing "ExpenseData" functionality
 const ExpenseItem = (props) => {
+  // This will not be call because => "App.js" is called after the "clickHandler"
+  let title = props.title;
+
   const clickHandler = () => {
-    console.log("Yoo");
+    title = "Updated!";
+    console.log(title);
   };
 
   return (
