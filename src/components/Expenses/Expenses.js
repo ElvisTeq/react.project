0d3c9597fd3462f092c1d7adc66ec8cd578cd98d
elvis <cenelvis@gmail.com>
2022-05-21 23:58:03 -0400

    Conditional Return Statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import React, { useState } from "react";
 import "./Expenses.css";
 import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("2020");
@@ -16,20 +16,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
-  // **************** Using conditions in Components ****************
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredYears.length > 0) {
-    expensesContent = filteredYears.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   // This works because we added "ExpenseItem(prop)"
   // selected={filterYear} => Sending "select" default value to "ExpensesFilter.js"
   return (
@@ -39,7 +25,7 @@ const Expenses = (props) => {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesList items={filteredYears} />
       </Card>
     </div>
   );
