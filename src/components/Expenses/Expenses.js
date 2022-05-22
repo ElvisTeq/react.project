4e7ab79174d9f8074db728b89de0b9f0891f92eb
elvis <cenelvis@gmail.com>
2022-05-21 23:29:08 -0400

    Outputting Conditional Content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,20 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filterYear;
   });
 
+  // **************** Using conditions in Components ****************
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredYears.length > 0) {
+    expensesContent = filteredYears.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   // This works because we added "ExpenseItem(prop)"
   // selected={filterYear} => Sending "select" default value to "ExpensesFilter.js"
   return (
@@ -25,14 +39,7 @@ const Expenses = (props) => {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredYears.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
