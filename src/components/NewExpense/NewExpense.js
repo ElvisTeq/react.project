94fc4bf8ee3cad3433fc00ea6abab4e656db723b
elvis <cenelvis@gmail.com>
2022-05-22 22:42:05 -0400

    Creating interactive buttons to the Form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   // Function to save data (which we going to use to get data from another Component)
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -10,13 +12,36 @@ const NewExpense = (props) => {
       id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
+
+    // Hide form after Submitting
+    setIsEditing(false);
+  };
+
+  // Challenge 4
+
+  // To show form after clicking
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  // To hide form after clicking
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   // "onSaveExpenseData" => is the function we gonna call in "ExpenseForm.js"
   // Which is going to call the function from this Componen "saveExpenseDataHandler" using (props.onSaveExpenseData())
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
